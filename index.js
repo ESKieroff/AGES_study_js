015feab0b7cb6db8b8176aebbc543679a43e21c5
ESkieroff <rodrigo.rosa81@edu.pucrs.br>
2022-04-19 01:01:47 -0300

    criando um servidor

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res)=>{
+    res.statusCode =200;
+    res.setHeader('Content-Type','text/plain');
+
+    res.end('Hello, Fellas!');
+});
+
+server.listen(port,hostname,()=>{
+    console.log('server online');
+});
+/*
 console.log("Hello, Felas");
 function somar(n1,n2){
     console.log(n1+n2);
@@ -5,3 +20,4 @@ function somar(n1,n2){
 setInterval(function(){
     somar(100,53);
 },2000);
+*/
